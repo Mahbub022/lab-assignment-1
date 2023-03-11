963e6d3a0d35781d7b00e42d65a9eab81e308925
unknown <mdbappy5960@gmail.com>
2023-03-11 23:52:11 +0600

    I have created a new java file named lab01.java

---
+++ app/src/main/java/com/example/mysimplenotif/lab01.java
@@ -0,0 +1,27 @@
+package com.example.mysimplenotif;
+
+public class lab01 {
+    int roll;
+    String name;
+
+    public lab01(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
